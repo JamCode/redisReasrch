4135a041c77057dc34279ce7553a551bc7083ce9
wanghan <wh85125@163.com>
2016-03-02 10:56:57 +0800

    #add redis

--- database/operation.js
+++ database/operation.js
@@ -1,1 +1,7 @@
 var conn = require('../utility.js');
+
+
+exports.loadEntity = function(callback){
+    var sql = 'select *from trdx_entity_master';
+    conn.executeSql(sql, [], callback);
+}
--- server.js
+++ server.js
@@ -1,2 +1,1 @@
 var operation = require('./database/operation.js');
-var operation1 = require('./database/operation.js');
