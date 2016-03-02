3346cdd84f6c1795dcb0f5b336ed108494731277
wanghan <wh85125@163.com>
2016-03-02 10:58:43 +0800

    load entity

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 var operation = require('./database/operation.js');
+
+
+operation.loadEntity(function(flag, result){
+    if(flag){
+        result.forEach(function(e){
+            console.log(e);
+        });
+    }else{
+        console.log(result);
+    }
+})
