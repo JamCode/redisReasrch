3c713677fa49a4e6f5dfb6678712b3046d17ddbc
wanghan <wh85125@163.com>
2016-03-02 11:11:12 +0800

    #add redis

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var operation = require('./database/operation.js');
+var redis = require("redis"),
+var client = redis.createClient({auth_pass:'here_dev'});
 
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+var entyBaseHash = 'entyBaseHash';
 
 operation.loadEntity(function(flag, result){
     if(flag){
-        result.forEach(function(e){
-            console.log(e);
+        client.del(entyBaseHash, function(err, reply){
+            if(err){
+                console.log(err);
+            }else{
+                result.forEach(function(e){
+                    client.hset(entyBaseHash, e.ema_enty_srno, e, redis.print);
+                });
+            }
         });
+
     }else{
         console.log(result);
     }
